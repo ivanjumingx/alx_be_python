dd1cd2b1907d38abd5aeae1332c41127664ce442
Mingle <bevolutionstudios@gmail.com>
2024-06-26 11:05:09 -0700

    Updated match_case_calculator.py script

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,28 +1,24 @@
-# Prompt the  user for input
+# Prompt for User Input
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
-
-# Ask for the type of  operation
 operation = input("Choose the operation (+, -, *, /): ")
 
-# Perform calculation based on the type of operation using if-elif-else
-result = 0
-error_message = "Cannot divide by zero."
-
-if operation == '+':
-    result = num1 + num2
-    print(f"The result is {result}.")
-elif operation == '-':
-    result = num1 - num2
-    print(f"The result is {result}.")
-elif operation == '*':
-    result = num1 * num2
-    print(f"The result is {result}.")
-elif operation == '/':
-    if num2 == 0:
-        print(error_message)
-    else:
-        result = num1 / num2
+# Perform the Calculation Using Match Case
+match operation:
+    case '+':
+        result = num1 + num2
         print(f"The result is {result}.")
-else:
-    print(f"Invalid operation '{operation}'. Please choose from '+', '-', '*', '/'.")
+    case '-':
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case '*':
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case '/':
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
+            print(f"The result is {result}.")
+    case _:
+        print(f"Invalid operation '{operation}'. Please choose from '+', '-', '*', '/'.")
